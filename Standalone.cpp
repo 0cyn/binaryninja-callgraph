a910ee5c0d76e36bbd2bd38c0ea752f824a2eaa4
kat <kat@vector35.com>
2023-07-07 17:09:41 -0400

    Fix

--- Standalone.cpp
+++ Standalone.cpp
@@ -5,3 +5,17 @@
 #include "UI/Callgraph/Callgraph.h"
 #include <binaryninjaapi.h>
 #include "uitypes.h"
+
+
+extern "C" {
+
+BN_DECLARE_CORE_ABI_VERSION
+BN_DECLARE_UI_ABI_VERSION
+
+BINARYNINJAPLUGIN bool UIPluginInit() {
+    CallgraphToolInit();
+
+    return true;
+}
+
+}
